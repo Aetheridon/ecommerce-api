00603f86bca8e13c6e54c09426f09b5b29e573dd
Aetheridon <lewisvr@protonmail.com>
2025-03-19 21:30:46 +0000

    basic support for searching for entries within the db

--- src/api.js
+++ src/api.js
@@ -1,13 +1,33 @@
 const express = require("express")();
-
 const PORT = (process.env.PORT || 8000);
+
+const products = require("./db/products.json");
 
 express.get("/products", (req, res) => {
     try {
-        res.send("Working!");
+        res.send(products);
     } catch (error) {
         res.status(500).end(`got error: $error}`);
     }
+});
+
+express.get("/products/:id", (req, res) => {
+    const productID = parseInt(req.params.id)
+
+    if (isNaN(productID)) {
+        return res.status(400).send("please provide a valid product ID!");
+    }
+
+    const product = products.find((p) => {
+        return p.id === productID;
+    });
+
+    if (product) {
+        res.json(product);
+    } else {
+        res.status(404).send(`Couldn't find an entry with id ${productID}!`);
+    }
+
 });
 
 express.listen(PORT, () => {
