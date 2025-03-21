7bfdd2d63c7e3c5dd5678a5bcf234a5ad247ed08
Aetheridon <lewisvr@protonmail.com>
2025-03-21 19:42:30 +0000

    added ability to update existing product

--- src/api.js
+++ src/api.js
@@ -1,12 +1,12 @@
 const express = require("express")();
-const bodyParser = require("body-parser");
-
+const bodyParser = require("body-parser"); 
 const fs = require('fs');
 const path = require('path');
 
-const PORT = (process.env.PORT || 8000);
 
 const products = require("./db/products.json");
+
+const PORT = (process.env.PORT || 8000);
 
 express.use(bodyParser.json());
 express.use(bodyParser.urlencoded({ extended: true}));
@@ -38,7 +38,7 @@ express.get("/products/:id", (req, res) => {
 
 });
 
-express.post("/products/add_item", (req, res) => {
+express.post("/products/add_item", async (req, res) => {
     const { name, description, price, category, stock, rating, vendor } = req.body;
     const requiredFields = [ name, description, price, category, stock, rating, vendor]; // not best if Schema changes, look into better methods for checking null fields.
 
@@ -71,6 +71,49 @@ express.post("/products/add_item", (req, res) => {
             console.log(`got error: ${error}`);
             res.status(400).send(`failed trying to write product to file: ${error}`);
         }
+    }
+
+});
+
+express.post("/update/:id", (req, res) => {
+    try {
+        const idToUpdate = parseInt(req.params.id) - 1; // to account for indexing at 0.
+
+        if (!products[idToUpdate]) {
+            return res.status(404).send("Product not found");
+        }
+
+        product = products[idToUpdate];
+
+        for (let key in req.body) {
+
+            if (!(key in product)) {
+                return res.status(400).send(`Invalid key: ${key} does not exist in product`);
+            }
+
+            if (key === "price" || key === "rating") {
+                product[key] = parseFloat(req.body[key]);
+            } else if (key === "stock") {
+                product[key] = parseInt(req.body[key]);
+            } else {
+                product[key] = req.body[key];
+            }
+        }
+
+        const filePath = path.join(__dirname, "db", "products.json");
+
+        fs.writeFile(filePath, JSON.stringify(products, null, 2), (err) => {
+            if (err) {
+                console.log(`Error writing to file: ${err}`);
+                res.status(500).send(`Got error trying to write to product to file: ${err}`);
+            } else {
+                res.status(200).send("updated db successfully!");
+            }
+        });
+
+    } catch (error) {
+        console.log(`got error: ${error}`);
+        res.status(400).send(`failed trying to write product to file: ${error}`);
     }
 
 });
